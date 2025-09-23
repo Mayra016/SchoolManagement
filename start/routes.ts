dde36ffb720051e6e9c5d87b421f08f95cb5d7e3
Mayra016 <mayrazucaro@gmail.com>
2025-09-23 19:29:06 -0300

    Feature: define app routes

--- start/routes.ts
+++ start/routes.ts
@@ -8,9 +8,43 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import StudentController from '#controllers/StudentController'
+import Controller from '#controllers/Controller'
 
 router.get('/', async () => {
   return {
     hello: 'world',
   }
 })
+
+// COMMON ROUTES
+router.post('/registrate', [Controller, 'registrate'])
+
+router.delete('/delete/:id', [Controller, 'delete'])
+
+router.get('/profile/:id', [Controller, 'profile'])
+
+
+// STUDENT ROUTES
+router.patch('/student/edit', [StudentController, 'edit'])
+
+router.delete('/student/delete', [StudentController, 'delete'])
+
+router.get('/student/cronogram', [StudentController, 'getCronogram'])
+
+// PROFESOR ROUTES
+router.patch('/professor/edit', [ProfessorController, 'edit'])
+
+router.post('/professor/create/classroom', [ProfessorController, 'createClassroom'])
+
+router.patch('/professor/edit/classroom', [ProfessorController, 'editClassroom'])
+
+router.delete('/professor/delete/classroom/:id', [ProfessorController, 'deleteClassroom'])
+
+router.get('/professor/get/classroom', [ProfessorController, 'getClassroom'])
+
+router.patch('/professor/add/student/:student_id/classroom/:classroom_id', [ProfessorController, 'addStudentToClassroom'])
+
+router.delete('/professor/remove/student/:student_id/classroom/:classroom_id', [ProfessorController, 'removeStudentFromClassroom'])
+
+router.get('/professor/students-from-classroom', [ProfessorController, 'getAllStudentsFromClassroom'])
