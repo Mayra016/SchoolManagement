1230bb95a12d3ce3f0c9e7343deb96a5d52e5a3c
Mayra016 <mayrazucaro@gmail.com>
2025-09-23 18:33:51 -0300

    Create project basic structure

---
+++ app/models/user.ts
@@ -0,0 +1,45 @@
+import { DateTime } from 'luxon'
+import hash from '@adonisjs/core/services/hash'
+import { compose } from '@adonisjs/core/helpers'
+import { BaseModel, column, beforeCreate } from '@adonisjs/lucid/orm'
+import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
+import { Role } from '../enums/roles.js'
+
+const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
+  uids: ['email'],
+  passwordColumnName: 'password',
+})
+
+export default class User extends compose(BaseModel, AuthFinder) {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare fullName: string | null
+
+  @column()
+  declare email: string
+
+  @column({ serializeAs: null })
+  declare password: string
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime | null
+
+  @column()
+  declare registration: number
+
+  @column.dateTime()
+  declare birthday: DateTime
+
+  @column()
+  declare role: Role
+
+  @beforeCreate()
+  public static assignRegistration(user: User) {
+    user.registration = Math.floor(Math.random() * 1000000)
+  }
+}
