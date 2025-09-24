fe7333e95456328f1c0578d3629fdf23d5add004
Mayra016 <mayrazucaro@gmail.com>
2025-09-24 11:42:31 -0300

    Feature: add functionality to get user profile, delete users, and edit users in UserService and UserRepository

--- app/repositories/UserRepository.ts
+++ app/repositories/UserRepository.ts
@@ -1,15 +1,32 @@
 import db from '@adonisjs/lucid/services/db'
 import UserI from '../interfaces/UserI.js'
+import User from '#models/user'
 
 
 export default class UserRepository {
+
+
     public trx: any
 
-    public async registerUser(newUser: UserI) {
+    public async getUser(id: number): Promise<User> {
         if (!this.trx) {
             this.trx = await db.transaction()
         }
-        await this.trx.insertQuery().table('users').insert(newUser)
+        return await this.trx.from('users').where('id', id).first()
+    }
+    
+    public async deleteUser(id: any) {
+        if (!this.trx) {
+            this.trx = await db.transaction()
+        }
+        await this.trx.from('users').where('id', id).delete()
+    }
+
+    public async saveUser(newUser: UserI) {
+        if (!this.trx) {
+            this.trx = await db.transaction()
+        }
+        await this.trx.insertQuery().table('users').insert(newUser).onConflict('id').merge()
     }
 
     public async startTransaction() {
--- app/services/UserService.ts
+++ app/services/UserService.ts
@@ -4,30 +4,73 @@ import UserRepository from "../repositories/UserRepository.js";
 import UserI from "../interfaces/UserI.js";
 import vine from '@vinejs/vine'
 import { userValidator } from "#validators/UserValidator";
+import User from "#models/user";
 
 export default class UserService {
     userRepository: UserRepository = new UserRepository();
 
-    edit(request: Request) {
-      
+    async edit(request: Request) {
+      try {
+        const validated: UserI | any = await request.validateUsing(userValidator)
+        await this.userRepository.saveUser(validated)
+        return { message: 'User edited successfully' }
+      } catch (error) {
+        if (error.status === 404) {
+          return {
+            message: 'User not found',
+            errors: error.messages,
+          }
+        } else {
+          return {
+            message: 'Unknow error',
+            errors: error.messages, 
+          }
+        }
+
+      }      
     }
 
     async getSchedule(student_id: any): Promise<Schedule> {
       throw new Error('Method not implemented.');
     }
 
-    profile(id: any): Object {
-        throw new Error("Method not implemented.");
+    async profile(id: any): Promise<User|Object> {
+      try {
+        const user: User = await this.userRepository.getUser(id)
+        return user
+      } catch (error) {
+        if (error.status === 404) {
+          return {
+            message: 'User not found',
+            errors: error.messages,
+          }
+        } else {
+          return {
+            message: 'Unknow error',
+            errors: error.messages, 
+          }
+        }
+
+      }
     }
 
-    delete(id: any) {
-        throw new Error("Method not implemented.");
+    async delete(id: any): Promise<Object> {
+      try {
+        await this.userRepository.deleteUser(id)
+        return { message: 'User deleted successfully' }
+      } catch (error) {
+        return {
+          status: 500,
+          message: 'Unknow error',
+          errors: error.messages, 
+        }
+      }
     }
 
-    async register(request: Request) {
+    async register(request: Request): Promise<Object> {
       try {
         const validated: UserI | any = await request.validateUsing(userValidator)
-        await this.userRepository.registerUser(validated)
+        await this.userRepository.saveUser(validated)
         return { message: 'User registered successfully' }
       } catch (error) {
         if (error.status === 422) {
