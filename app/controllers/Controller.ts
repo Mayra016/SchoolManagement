dfa59f528b9fa139eb223b2f947236f2de4089a2
Mayra016 <mayrazucaro@gmail.com>
2025-09-23 21:32:06 -0300

    Feature: add Controller endpoints to handle user generic petitions

---
+++ app/controllers/Controller.ts
@@ -0,0 +1,50 @@
+import UserService from "#services/UserService";
+import { HttpContext } from "@adonisjs/core/http";
+
+
+export default class Controller {
+    userService: UserService = new UserService();
+
+    public async registrate({ response, request }: HttpContext) {
+        try {
+            this.userService.registrate(request)
+            return response.status(200).json({
+              message: 'Student registered successfully',
+            })
+        }
+        catch(e: any){
+            return response.status(500).json({
+              message: 'Error registrating user',
+            })
+        }
+    } 
+
+    public async delete({ response, params }: HttpContext) {
+        try {
+            this.userService.delete(params.id);
+            return response.status(200).json({
+              message: 'Success deleting user',
+            })
+        }
+        catch(e: any) {
+            return response.status(500).json({
+                message: 'Error deleting user',
+              })
+        }
+    }   
+
+    public async profile({ response, params }: HttpContext) {
+        try {
+            let info: Object = this.userService.profile(params.id);
+            return response.status(200).json({
+              info
+            })
+        }
+        catch (e: any) {
+            return response.status(500).json({
+                message: 'Error getting profile',
+              })
+        }
+    }   
+
+}
